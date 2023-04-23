26ba665d8df5198bdacd52a2a7a55eee41d6cd43
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-04-23 00:38:38 -0300

    PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="apple-touch-icon" href="/icon.png"/>
+        <meta name="theme-color" content="#464747"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
